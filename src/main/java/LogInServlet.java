8e6da88ccb6bcca02eb78c984e8cae0b89e10469
hallursimonsen <hallur3395@gmail.com>
2020-03-17 16:02:53 +0100

    login er sgu nok ved at være ok

--- src/main/java/LogInServlet.java
+++ src/main/java/LogInServlet.java
@@ -1,16 +1,43 @@
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "LogInServlet", urlPatterns = {"/LogInServlet"})
 public class LogInServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.getWriter().println("Du har ramt LogInServletten");
+        ServletContext servletContext = getServletContext();
 
+        if(servletContext.getAttribute("brugerMap") == null);
+
+        String navn = request.getParameter("navn");
+        String kodeord = request.getParameter("kodeord");
+
+
+        Map<String, String> brugerMap = new HashMap<>();
+
+        brugerMap.put("test","test");
+
+        servletContext.setAttribute("brugerMap", brugerMap);
+
+        if(!((Map<String,String>)servletContext.getAttribute("brugerMap")).containsKey(navn)){
+            request.setAttribute("besked", "brugernavnet findes ikke");
+            request.getRequestDispatcher("index.jsp").forward(request,response);
+        }
+
+        if( ((Map<String,String>)servletContext.getAttribute("brugerMap")).get(navn).equalsIgnoreCase(kodeord)){
+
+            request.getRequestDispatcher("WEB-INF/HuskeListe.jsp").forward(request,response);
+        }
+
+        request.setAttribute("besked", "din kode var forkert, prøv igen");
+        request.getRequestDispatcher("index.jsp").forward(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ src/main/webapp/WEB-INF/HuskeListe.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hallursimonsen
+  Date: 3/17/20
+  Time: 15:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Huskeliste</title>
+</head>
+<body>
+
+du har nu adgang til din huskeliste
+
+</body>
+</html>
